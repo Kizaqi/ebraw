2f4ee93c31abb21578977b675b61e2b2c33cab84
Mykhailo Luzhetskyi <luzeckijmihajlo0@gmail.com>
2023-03-21 10:46:08 +0200

    commit

---
+++ dist/js/script.js
@@ -0,0 +1,54 @@
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+  let links = menu.find('.burger-menu_link');
+  let overlay = menu.find('.burger-menu_overlay');
+
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass('burger-menu_active');
+
+    if (menu.hasClass('burger-menu_active')) {
+      $('body').css('overlow', 'hidden');
+    } else {
+      $('body').css('overlow', 'visible');
+    }
+  }
+}
+$('.owl-carousel').owlCarousel({
+  animateOut: 'fadeOut',
+  loop: true,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+var owl = $('.owl-carousel');
+owl.owlCarousel();
+
+$('.next').click(function () {
+  owl.trigger('next.owl.carousel');
+});
+
+$('.previous').click(function () {
+  owl.trigger('prev.owl.carousel');
+});
+
+burgerMenu('.burger-menu');
